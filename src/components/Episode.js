83603989c54a9f614e6a58d74caf3aab60418851
jaskrrish <jashehz@gmail.com>
2024-06-22 00:02:04 +0530

    Update Podcase Section

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import { SocialIcon } from "react-social-icons"
+import React from "react";
+import { SocialIcon } from "react-social-icons";
 
-export default function Episode(props) {
+export default function Episode({ title, image }) {
   return (
-    <div className='p-5 my-2 bg-[#1e2320] flex flex-row rounded-lg'>
-         <img
-            className='w-30 h-60 mt-7'
-            src='/images/podcast is out post.png'
-            alt='/'
-         />
-         <div className='m-5 p-5'>
-          <h1 className='text-red-500 text-3xl font-bold mb-10'>{props.title}</h1>
-          <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <div className='flex flex-row space-x-5 mt-5'>
-           <SocialIcon
-            className='text-lg'
-            url='https://www.youtube.com/channel/UC5IVJQuG2iFmysir2RNRVSw'
+    <div className="p-5 my-2 bg-[#1e2320] flex flex-row rounded-lg">
+      <img className="w-30 h-60 mt-7" src={image} alt="/" />
+      <div className="m-5 p-5">
+        <h1 className="text-red-500 text-3xl font-bold mb-10">{title}</h1>
+        <p className="text-white">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div className="flex flex-row space-x-5 mt-5">
+          <SocialIcon
+            className="text-lg"
+            url="https://www.youtube.com/channel/UC5IVJQuG2iFmysir2RNRVSw"
             fgColor="#eb4034"
             bgColor="transparent"
-           />
-           <SocialIcon
-            url='https://open.spotify.com/show/0RlQigGr31JbgjdkIVbPZY'
+          />
+          <SocialIcon
+            url="https://open.spotify.com/show/0RlQigGr31JbgjdkIVbPZY"
             fgColor="#1ED760"
             bgColor="transparent"
           />
-          </div>
-         </div>
-         
-       </div>
-  )
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,27 +1,93 @@
-import React from 'react'
-import { SocialIcon } from "react-social-icons"
-import Episode from './Episode'
+import React from "react";
+import Slider from "react-slick";
+import Episode from "./Episode";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Podcast() {
   return (
-    <div className='max-w-7xl mx-auto z-20 p-5 items-center flex flex-col'>
-      <img
-        className='h-40 w-30 relative'
-        src='/images/track degin.png'
-        alt='/'
-      />
-      <h1 className='text-2xl text-center text-white absolute my-20 py-5'>OUR PODCASTS</h1>
-     
-      <div className='max-w-fit'>
-       <Episode
-        title='Episode 1 ft. Prayasu'
-        image=''
+    <>
+      <div className="max-w-7xl mx-auto z-20 p-5 items-center flex flex-col">
+        <img
+          className="h-40 w-30 relative"
+          src="/images/track degin.png"
+          alt="/"
+        />
+        <h1 className="text-2xl text-center text-white absolute my-20 py-5">
+          OUR PODCASTS
+        </h1>
 
-       />
-       <Episode/>
-       <Episode/>
-       <Episode/>
+        <div className=""></div>
       </div>
-    </div>
-  )
+      <Carasouel />
+    </>
+  );
+}
+
+function Carasouel() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    nextArrow: <ChevronRight color="#ffffff" strokeWidth={6} size={220} />,
+    prevArrow: <ChevronLeft color="#ffffff" strokeWidth={6} size={220} />,
+    cssEase: "linear",
+  };
+  return (
+    <>
+      <div className="">
+        <div className="max-w-fit px-7">
+          <Slider {...settings}>
+            <div className="mx-2">
+              <Episode
+                title="Episode 1 ft. Prayasu"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/Prayasu Thumbnail.png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 2 ft. Zeeshan"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/Zeeshan Thumbnail.png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 3 ft. Janvi"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/thumbnail.png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 4 ft. Waseem"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/thumbnail - Copy.png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 5 ft. Hrushi"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/EPISODE 1(1).png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 6 ft. Paras"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/thumbnail.png"
+              />
+            </div>
+            <div className="mx-2">
+              <Episode
+                title="Episode 6 ft. Achintya"
+                image="/images/sarthak bhaiya deklo-20240621T141842Z-001/sarthak bhaiya deklo/EPISODE 1.png"
+              />
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
 }
