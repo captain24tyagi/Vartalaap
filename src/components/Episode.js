2c556b3fe487646ee6d0a9c0bf7936da22c7ae8b
jaskrrish <jashehz@gmail.com>
2024-06-29 16:07:08 +0530

    Update Episode component height for better display consistency

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -3,7 +3,7 @@ import { SocialIcon } from "react-social-icons";
 
 export default function Episode({ title, image, content, youtube, spotify }) {
   return (
-    <div className="p-5 min-w-full md:h-[23rem] h-[35rem] my-2 bg-[#121212] drop-shadow-[4px_4px_rgba(255,46,84,0.5)] flex flex-col md:flex-row rounded-lg">
+    <div className="p-5 min-w-full md:h-[23rem] h-[36rem] my-2 bg-[#121212] drop-shadow-[4px_4px_rgba(255,46,84,0.5)] flex flex-col md:flex-row rounded-lg">
       <img className="md:w-30 md:h-60 mt-7 h-[180px]" src={image} alt="/" />
       <div className="m-5 p-5">
         <h1 className="text-red-500 text-xl md:text-3xl font-bold mb-10">
